c4b3c4c3c953e06e27c2a113182376e911d4a033
BrandonMok <brandonmok88@yahoo.com>
2022-06-09 17:32:00 -0400

    Adjusting / figuring out layouts

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Homepage() {
     return (
-        <div className='home'>
-            <h1>this is the homepage</h1>
+        <div className='container home'>
+            <div className='home__intro'>
+                <h1>ACNH Nookipedia</h1>
+            </div>
         </div>
     );
 }
