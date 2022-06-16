c503143dd4056eab12c4c304d40ea60d1999f47c
BrandonMok <brandonmok88@yahoo.com>
2022-06-16 15:22:30 -0400

    Mobile Nav - changed to adding modifier classes

--- src/components/mobilenav.js
+++ src/components/mobilenav.js
@@ -1,28 +1,19 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from 'react-router-dom';
 
 export default function MobileNav() {
     const [navShow, updateNavShow] = useState(false);
     const nav = useRef();
 
-    // not working...
-    useEffect(() => {
-        let percentWidth = navShow ? "45%" : "0";
-        nav.current.style.width = percentWidth;
-    }, [navShow]);
-
     return (
         <div className="mobile-nav">
             <span className="mobile-nav__hamburger" onClick={() => updateNavShow(prev => !prev)}>&#9776;</span>
-
-            <div ref={nav} className="mobile-nav__container">
-                <nav className="mobile-nav__container__links">
-                    <Link to="/">Home</Link>
-                    <Link to="/fish">Fish</Link>
-                    <Link to="/bugs">Bugs</Link>
-                    <Link to="/sea">Sea Creatures</Link>
-                </nav>
-            </div>
+            <nav ref={nav} className={ `mobile-nav__links ${navShow ? "mobile-nav__links--show" : "mobile-nav__links--hide"}` }>
+                <Link to="/">Home</Link>
+                <Link to="/fish">Fish</Link>
+                <Link to="/bugs">Bugs</Link>
+                <Link to="/sea">Sea Creatures</Link>
+            </nav>
         </div>
     );
 }
