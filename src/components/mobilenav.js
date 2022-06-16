1eb822e83e78fd41703a1c18df03ca62c949a3f9
BrandonMok <brandonmok88@yahoo.com>
2022-06-16 14:34:11 -0400

    mobile nav cont.

--- src/components/mobilenav.js
+++ src/components/mobilenav.js
@@ -1,21 +1,26 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 export default function MobileNav() {
     const [navShow, updateNavShow] = useState(false);
-    const mobileNav = useRef();
+    const nav = useRef();
 
-    function showMobileNav() {
-        updateNavShow(prev => !prev);
-    }
+    // not working...
+    useEffect(() => {
+        let percentWidth = navShow ? "45%" : "0";
+        nav.current.style.width = percentWidth;
+    }, [navShow]);
 
     return (
         <div className="mobile-nav">
-            <span className="mobile-nav__hamburger" onClick={showMobileNav}>&#9776;</span>
+            <span className="mobile-nav__hamburger" onClick={() => updateNavShow(prev => !prev)}>&#9776;</span>
 
-            <div ref={mobileNav} className="mobile-nav__container">
+            <div ref={nav} className="mobile-nav__container">
                 <nav className="mobile-nav__container__links">
                     <Link to="/">Home</Link>
+                    <Link to="/fish">Fish</Link>
+                    <Link to="/bugs">Bugs</Link>
+                    <Link to="/sea">Sea Creatures</Link>
                 </nav>
             </div>
         </div>
