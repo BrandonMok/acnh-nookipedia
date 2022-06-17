ddf10dec938c8b1f2a42637e6232b8c670cc8976
brandonmok <brandonmok88@yahoo.com>
2022-06-17 14:38:29 -0400

    Uncommented Search component

--- src/components/search.js
+++ src/components/search.js
@@ -4,10 +4,23 @@
  * TBD - Most likely will accept a prop for what area it is located on and to call the respective function
  */
 
-// import React from 'react';
+import React from 'react';
 
-// export default function Search() {
-//     return (
-        
-//     );
-// }
+export default function Search({list, updateFishList}) {
+    // take prop: list
+    // sort through this list and return the found item
+
+    function searchSubmit() {
+        // take search term, sanitize, and then look for object that matches it, return
+
+        // once item is found or not, call 'updateFishList" which is a function as a prop from parent
+        // this function (the updating State function) will update the state and update the UI
+    }
+
+
+    return (    
+        <form>
+            <input type="text" />
+        </form>
+    );
+}
--- src/pages/fish.js
+++ src/pages/fish.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect, useMemo} from 'react';
 import ItemGrid from '../components/itemgrid';
+import Search from '../components/search';
 
 export default function Fish() {
-    const [fishList, updateFishList] = useState([]);
+    let fishbackup = []; // will always keep ALL fish.. 
+    const [fishList, updateFishList] = useState([]);    // fishList state var will display all or one (Search cmp may change fishList)
     const apiData = useMemo(async () => fetchFishData(), []);
 
     async function fetchFishData() {
@@ -15,6 +17,7 @@ export default function Fish() {
         .then((resp) => {
             const respArr = Array.from(Object.entries(resp));
             updateFishList(respArr);
+            fishbackup = respArr;
         })
         .catch((error) => {
             console.log(error);
@@ -22,6 +25,9 @@ export default function Fish() {
     }, [apiData]);
 
     return (
-        <ItemGrid itemList={fishList} area="fish" />
+        <>
+            <Search list={fishList} updateList={updateFishList} />
+            <ItemGrid itemList={fishList} area="fish" />
+        </>
     )
 }
