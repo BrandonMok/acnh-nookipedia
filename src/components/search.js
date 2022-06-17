d15fe4f58a1cc5d17873e489f4a424d73cc252cc
brandonmok <brandonmok88@yahoo.com>
2022-06-17 19:26:13 -0400

    [SEARCH] Styling search bar and submit button

--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,12 @@
-/**
- * TODO:
- * Stubbed in component to handle searching of data from api
- * TBD - Most likely will accept a prop for what area it is located on and to call the respective function
- */
-
 import React from 'react';
 
 export default function Search({list, updateFishList}) {
     // take prop: list
     // sort through this list and return the found item
 
-    function searchSubmit() {
+    function handleSearch(e) {
+        e.preventDefault();
+
         // take search term, sanitize, and then look for object that matches it, return
 
         // once item is found or not, call 'updateFishList" which is a function as a prop from parent
@@ -18,9 +14,12 @@ export default function Search({list, updateFishList}) {
     }
 
 
-    return (    
-        <form>
-            <input type="text" />
-        </form>
+    return (   
+        <div className='container search'>
+            <form className='search__form'>
+                <input type="text" placeholder='Enter term here...' className='search__form__input' />
+                <button type="submit" onClick={handleSearch} className='search__form__submit'>Submit</button>
+            </form>
+        </div>
     );
 }
