4dcd19c0c3e7bd72733898abb5ca7950e8b70e93
BrandonMok <brandonmok88@yahoo.com>
2022-06-26 12:19:45 -0400

    [ITEMGRID] Changed from grid layout to flexbox

--- src/components/itemgrid.js
+++ src/components/itemgrid.js
@@ -11,30 +11,26 @@ import Card from './card';
  */
 export default function ItemGrid({itemList, area}) {
     return (
-        <div className='container item-grid'>
-            <div className='row'>
-                <div className='col-12 pt-4 pb-4 item-grid__list'>
-                    { itemList.length > 0 ? (
-                        itemList.map((item) => {
-                            let path = "/" + area +"/" + item[1].id;
+        <div className='container item-flexbox'>
+            { itemList.length > 0 ? (
+                itemList.map((item) => {
+                    let path = "/" + area +"/" + item[1].id;
 
-                            return (
-                                <Link to={path}>
-                                    <Card 
-                                        key={item[1].id}
-                                        name={item[1].name}
-                                        price={item[1].price}
-                                        icon_uri={item[1].icon_uri}
-                                        shadow={item[1].shadow}
-                                    />
-                                </Link>
-                            );
-                        })
-                    ) : (
-                        <h1>No {area} Available!</h1>
-                    )}
-                </div>
-            </div>
+                    return (
+                        <Link to={path}>
+                            <Card 
+                                key={item[1].id}
+                                name={item[1].name}
+                                price={item[1].price}
+                                icon_uri={item[1].icon_uri}
+                                shadow={item[1].shadow}
+                            />
+                        </Link>
+                    );
+                })
+            ) : (
+                <h1>No Results Available!</h1>
+            )}
         </div>
     );
 }
