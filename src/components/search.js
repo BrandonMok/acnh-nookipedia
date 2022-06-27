ef5783633ac40595abcd881dfb211495eea33e83
BrandonMok <brandonmok88@yahoo.com>
2022-06-27 14:42:43 -0400

    Cleanup

--- src/components/search.js
+++ src/components/search.js
@@ -14,57 +14,49 @@ export default function Search({fullList, updateList}) {
         return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
     }
 
-    // [TODO] Fix performance here
-    // having search called on onChange causes it to fire multiple times...
-    function handleSearch(e) {
-        e.preventDefault();
+    function handleSearch() {
+        const searchTerm = textInput.current.value;
 
-        // Wait a bit before doing the filtering to give user time to enter full name.
-        // otherwise, code performance would hurt with the code filtering the list each time a letter is entered.
-        setTimeout(() => {
-            const searchTerm = textInput.current.value;
+        if (searchTerm !== "") {
+            let sanitizedInput = inputSanitize(searchTerm).trim();
 
-            if (searchTerm !== "") {
-                let sanitizedInput = inputSanitize(searchTerm).trim();
+            if (!isNaN(sanitizedInput)) {
+                // number value - only numeric value is the price
+                let userInputNum = parseInt(sanitizedInput);
 
-                if (!isNaN(sanitizedInput)) {
-                    // number value - only numeric value is the price
-                    let userInputNum = parseInt(sanitizedInput);
+                let filteredNumList = fullList.filter((item) => {
+                    let itemPrice = item[1].price;
+                    return itemPrice === userInputNum;
+                });
 
-                    let filteredNumList = fullList.filter((item) => {
-                        let itemPrice = item[1].price;
-                        return itemPrice === userInputNum;
-                    });
-
-                    updateList(filteredNumList);
-                }
-                else {
-                    // string values
-                    let validInput = sanitizedInput.toLowerCase();
-            
-                    let filteredList = fullList.filter((item) => {
-                        let itemName = item[1].name["name-USen"].toString().toLowerCase();
-                        let shadow = item[1].shadow ? item[1].shadow.toString().toLowerCase() : undefined;
-
-                        if (itemName.includes(validInput)) {
-                            return itemName.includes(validInput);
-                        }
-                        else if (shadow !== undefined && shadow.includes(validInput)) {
-                            return shadow.includes(validInput)
-                        }
-                        else {
-                            return null;
-                        }
-                    });
-
-                    updateList(filteredList);
-                }
+                updateList(filteredNumList);
             }
             else {
-                // empty string, need to reset list to full list...
-                updateList(fullList);
+                // string values
+                let validInput = sanitizedInput.toLowerCase();
+        
+                let filteredList = fullList.filter((item) => {
+                    let itemName = item[1].name["name-USen"].toString().toLowerCase();
+                    let shadow = item[1].shadow ? item[1].shadow.toString().toLowerCase() : undefined;
+
+                    if (itemName.includes(validInput)) {
+                        return itemName.includes(validInput);
+                    }
+                    else if (shadow !== undefined && shadow.includes(validInput)) {
+                        return shadow.includes(validInput)
+                    }
+                    else {
+                        return null;
+                    }
+                });
+
+                updateList(filteredList);
             }
-        }, 1500);
+        }
+        else {
+            // empty string, need to reset list to full list...
+            updateList(fullList);
+        }
     }
 
     return (   
