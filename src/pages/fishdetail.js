ec0ad90bebe72ca506152acd2425fe4f3e3e54f5
BrandonMok <brandonmok88@yahoo.com>
2022-07-02 13:47:13 -0400

    [FishDetail] Stubbing in fields that will display

--- src/pages/fishdetail.js
+++ src/pages/fishdetail.js
@@ -4,36 +4,76 @@ import { useParams } from "react-router";
 export default function FishDetail() {
     // ID passed in param
     const {id} = useParams();
-
+    
     const [fish, updateFish] = useState({});
-    const apiData = useMemo(async () => await fetchFish(), []);
+    const apiData = useMemo(async() => await fetchFish(), []);
 
     async function fetchFish() {
         const response = await fetch(`https://acnhapi.com/v1/fish/${id}`);
         return await response.json();
     }
 
+    let monthMap = new Map();
+    monthMap.set('1', 'January');
+    monthMap.set('2', 'February');
+    monthMap.set('3', 'March');
+    monthMap.set('4', 'April');
+    monthMap.set('5', 'May');
+    monthMap.set('6', 'June');
+    monthMap.set('7', 'July');
+    monthMap.set('8', 'August');
+    monthMap.set('9', 'September');
+    monthMap.set('10', 'October');
+    monthMap.set('11', 'November');
+    monthMap.set('12', 'December');
+
     useEffect(() => {
         apiData
         .then((resp) => {
-            updateFish(resp);
+            let northernAvail =  resp["availability"]["month-northern"];
+            let southernAvail =  resp["availability"]["month-southern"];
 
-            console.log(resp);
+            let northernNumArr = northernAvail.split('-');
+            let northernText = monthMap.get(northernNumArr[0]) + "-" + monthMap.get(northernNumArr[1]);
+
+            let southernNumArr = southernAvail.split('-');
+            let southernText = monthMap.get(southernNumArr[0]) + "-" + monthMap.get(southernNumArr[1]);
+
+            let updatedObj = {...resp, "availability": { ...resp["availability"], "month-northern": northernText, "month-southern": southernText}}
+            updateFish(updatedObj);
         })
         .catch((error) => {
             console.log(error);
         });
     }, [apiData]);
 
-    return (
-        <div className="container fish-detail">
-            <div className="fish-detail__image-container">
-                {/* <img src={fish["image_uri"]} alt={fish["name"]["name"]} /> */}
+
+    if (Object.keys(fish).length !== 0) {
+        return (
+            <div className="fish-detail">
+                <div className="fish-detail__container">
+                    <div className="fish-detail__container__image-container">
+                        <img src={ fish["image_uri"] } alt={ fish["name"]["name-USen"] } width="600px" />
+                    </div>
+                    <div className="fish-detail__container__fish-info">
+                        <h1>{ fish["name"]["name-USen"] }</h1>
+                        <p>Location: { fish["availability"]["location"] }</p>
+                        <p>Rarity: { fish["availability"]["rarity"] } </p>
+
+                        {fish["availability"]["time"] !== "" && <p>Time: { fish["availability"]["time"] } </p>}
+
+                        <p>Northern Availability: { fish.availability["month-northern"] }</p>
+                        <p>Southern Availability: { fish.availability["month-southern"] }</p>
+
+                        <p>{ fish["museum-phrase"] }</p>
+                    </div>
+                </div>
             </div>
-            <div className="fish-detail__fish-info">
-                <h1>{ fish["name"]["name-USen"] }</h1>
-                <p>{ fish["availability"]["location"] }</p>
-            </div>
-        </div>
-    );
+        );
+    }
+    else {
+        return(
+            <></>
+        );
+    }
 }
